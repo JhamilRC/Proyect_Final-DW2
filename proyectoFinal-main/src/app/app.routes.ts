3261639100ca95d67b0d535706150e34fa3e921f
JhamilRc <jhamilrocha11@gmail.com>
2024-07-03 12:52:38 -0400

    03/07/24

---
+++ proyectoFinal-main/src/app/app.routes.ts
@@ -0,0 +1,16 @@
+import { Routes } from '@angular/router';
+import { DetalleproductoComponent } from './pages/detalleproducto/detalleproducto.component';
+import { ListaproductoComponent } from './pages/listaproducto/listaproducto.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AddproductoComponent } from './pages/addproducto/addproducto.component';
+import { UpdproductoComponent } from './pages/updproducto/updproducto.component';
+import { TablaproductosComponent } from './pages/tablaproductos/tablaproductos.component';
+
+export const routes: Routes = [
+    { path: 'listarproductos', component: ListaproductoComponent },
+    { path: 'addproductos', component: AddproductoComponent },
+    { path: 'producto/:id', component: DetalleproductoComponent },
+    { path: 'tablaproductos', component: TablaproductosComponent },
+    { path: 'updproductos/:id', component: UpdproductoComponent },
+    { path: '**', component: InicioComponent },
+];
---
+++ proyectoFinal-main/src/app/pages/detalleproducto/detalleproducto.component.ts
@@ -0,0 +1,48 @@
+// detalleproducto.component.ts
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SproductosService } from '../../services/sproductos.service';
+import { ProcedureParam } from '../../models/procedureparam';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+@Component({
+  selector: 'app-cardproducto',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detalleproducto.component.html',
+  styleUrls: ['./detalleproducto.component.css']
+})
+export class DetalleproductoComponent implements OnInit {
+  parametros: ProcedureParam = new ProcedureParam();
+  producto: any = [];
+  imagenPrincipal: string = '';
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private _sproductosService: SproductosService
+  ) { }
+
+  ngOnInit(): void {
+    this.parametros.inicia();
+    const id = this.activatedRoute.snapshot.paramMap.get('id') || '0';
+    this.parametros.pCampo0 = 'Cidproducto';
+    this.parametros.pValor0 = id;
+
+    this._sproductosService.selProductos(this.parametros).subscribe((resp: any) => {
+      this.producto = resp[0];
+      this.imagenPrincipal = `../../../img/${this.producto.nombre}_1.png`;
+    });
+  }
+  cambiarImagenPrincipal(nuevaSrc: string): void {
+    this.imagenPrincipal = nuevaSrc;
+  }
+  comprar(){
+    Swal.fire({
+      position: "center",
+      icon: "success",
+      title: "Producto Comprado",
+      showConfirmButton: false,
+      timer: 2000
+    });
+  }
+}
